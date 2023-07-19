0c3f4a3ab28e0fbcae78f1ba416bed77513c2475
Rabiul-Hasan-work <rabiulworkapps@gmail.com>
2023-07-19 15:39:40 +0600

    added action hovers

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -3,6 +3,7 @@ package pages;
 import drivers.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -22,28 +23,29 @@ public class HomePage {
 
     @FindBy(css = "body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > div:nth-child(3) > div > div.single-products > div.productinfo.text-center > a")
     private WebElement addToCartFirst;
-
     @FindBy(css = "body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > div:nth-child(4) > div > div.single-products > div.productinfo.text-center > a")
     private WebElement addToCartSecond;
-
     @FindBy(css = "#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(3) > a")
     private WebElement cart;
-
     @FindBy(css = "#cartModal > div > div > div.modal-body > p:nth-child(2) > a")
     private WebElement viewCart;
     @FindBy(css = "#cart_info_table > thead > tr > td.image")
     private WebElement cartTable;
-
     @FindBy(css = "#do_action > div.container > div > div > a")
     private WebElement proceedToCheckButton;
-
     @FindBy(css = "#cartModal > div > div > div.modal-footer > button")
     private WebElement continueShoppingButton;
     @FindBy(css = "#header > div > div > div > div.col-sm-4 > div > a > img")
     private WebElement homePageIcon;
+    @FindBy(css = "body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > div:nth-child(3) > div > div.single-products > div.productinfo.text-center")
+    private WebElement firstElement;
+    @FindBy(css = "body > section:nth-child(3) > div > div > div.col-sm-9.padding-right > div.features_items > div:nth-child(4) > div > div.single-products > div.productinfo.text-center")
+    private WebElement secondElement;
 
 
     public void addFirstElementToCart() {
+        Actions hover = new Actions(driver);
+        hover.moveToElement(firstElement).build().perform();
         addToCartFirst.click();
         int timeoutInSeconds = Constants.TIMEOUT;
         Duration timeout = Duration.ofSeconds(timeoutInSeconds);
@@ -59,6 +61,8 @@ public class HomePage {
     }
 
     public void addSecondElementToCart() {
+        Actions hover = new Actions(driver);
+        hover.moveToElement(secondElement).build().perform();
         addToCartSecond.click();
         viewCart.click();
         int timeoutInSeconds = Constants.TIMEOUT;
