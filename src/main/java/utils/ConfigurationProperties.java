8884cf6f47053f7266b74f6d79d29f4465c32d5e
Rabiul-Hasan-work <rabiulworkapps@gmail.com>
2023-07-25 11:53:59 +0600

    added springframework support

---
+++ src/main/java/utils/ConfigurationProperties.java
@@ -0,0 +1,35 @@
+package utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+@PropertySource("framework.properties")
+public class ConfigurationProperties {
+
+    @Value("${browser}")
+    private String browser;
+    @Value("${email}")
+    private String email;
+    @Value("${password}")
+    private String password;
+    @Value("${NameOnCard}")
+    private String NameOncard;
+    @Value("${cardNumber}")
+    private String cardNumber;
+    @Value("${cvc}")
+    private String cvc;
+    @Value("${expirationMM}")
+    private String expirationMM;
+    @Value("${expirationYY")
+    private String expirationYY;
+    @Value("${username}")
+    private String username;
+
+
+}
